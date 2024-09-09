a2c964bae9acc7a1c8a2391cd7f1334e290359b5
ayomide <ahmedayomide0412@gmail.com>
2024-09-09 13:41:05 +0100

    main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello World!
+      </h1>
+      <p>The best of the best is here</p>
+    </div>
+  );
+}
+
+export default App;
